a49c3bcc5166ce53382b045d1ff611f518d495b7
luvagu <luiavag@gmail.com>
2021-04-18 01:16:29 -0300

    protected route

--- pages/api/updateLikes.js
+++ pages/api/updateLikes.js
@@ -1,16 +1,10 @@
-import { withApiAuthRequired, getSession } from '@auth0/nextjs-auth0'
 import { updateDocLikes } from '../../utils/fauna.helpers'
 
-export default withApiAuthRequired(async function handler(req, res) {
-	const { user } = getSession(req, res)
+export default async function handler(req, res) {
 	const { ref, userId } = req.body
 
 	if (req.method !== 'PUT') {
 		return res.status(405).json({ message: 'Method not allowed' })
-	}
-
-	if (userId !== user.sub) {
-		return res.status(403).json({ message: 'Unauthorised' })
 	}
 
 	try {
@@ -20,4 +14,4 @@ export default withApiAuthRequired(async function handler(req, res) {
 		console.log('Error: %s', error?.message)
 		res.status(500).json({ message: 'Something went wrong.' })
 	}
-})
+}
